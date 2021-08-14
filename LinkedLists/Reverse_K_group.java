d475378f4e7daa59e47efc67fe4c60a61d107393
Sudhanshu Kumar <63813872+SudhanshuBlaze@users.noreply.github.com>
2021-08-14 21:50:18 +0530

    +

--- LinkedLists/Reverse_K_group.java
+++ LinkedLists/Reverse_K_group.java
@@ -1,6 +1,10 @@
 /*
 Reverse group of size 'k' in a LinkedList
+
+Approach of nested for-loop: we need to reverse k-1 links. So, we start from k=1 
+Ex.when k=3, list=1->2->3, there are 2 links to be reversed
 */
+
 class Reverse_K_group{
 	public static void main(String args[]){
 		LinkedList list = new LinkedList();
@@ -19,19 +23,37 @@ class Reverse_K_group{
 		if(head==null || k==1) return null;  //corner case
 		Node dummy=new Node(0);
 		dummy.next = head;
-
+		//Initially prev is at dummy node, so in the first iteration whenever the value of
+		// prev.next changed dummy's value is also changed
+		// after the first iteration prev=curr and curr moves one step forward.
 		Node curr=head, prev=dummy, nex=dummy;
 		int count=0;
-		for(curr=head; curr!= null; curr=curr.next) count++;
+		for(curr=head; curr!= null; curr=curr.next) count++; //size of linked list
 
 		while(count>=k){
-			curr = prev.next;   //first element of the group
-			nex=curr.next;		//second element of the group
-			/*we need to reverse k-1 links. So, we start from k=1
-			Ex. when k=3, list=1->2->3, there are 2 links to be reversed*/
+			//curr points to first element of the group for entire iteration of that grp
+			curr = prev.next; //don't do curr=curr.next because
+			// initially it's pointing to null, due to previous size count for-loop
+
+			//nex pointer moves in between iterations
+			nex=curr.next;
 			for(int i=1;i<k;i++){
-				
+				/*Note: in this loop, curr and prev pointers are not moving
+				just there .next pointer is moving by 1 step at each iteration
+				curr.next and prev.next is used for preserving the location*/
+
+				curr.next =nex.next;  //storing the nex.next pointer which will be 
+				//assigned to nex at the end, this step also helps in reversing the group
+				nex.next=prev.next;  // Main-reversal process
+				prev.next = nex;	
+				nex=curr.next;     //moves forward using curr.next pointer
 			}
+			prev=curr;
+			count-=k;
+			/*on reversing curr becomes the last element of the grp, so we 
+			point prev to last element of the grp(which is curr) and move curr to next
+			Ex. (curr)1->2->3->4 on reversal 3->2->1(curr)->4 
+			on next step 3->2->1(prev)->(curr)4 */
 		}
 		return dummy.next;
 	}
--- replacePI_recur.java
+++ replacePI_recur.java
@@ -1,16 +1,16 @@
 class replacePI_recur{
 	public static void main(String[] args){
-        String s = "pipppiiipi";
-
+        String s = "pipiipipp";
         System.out.println(replacePi(s));
     }
     static String replacePi(String s){
     	if(s.length()<=1)
     		return s;
     	
-    	if(s.substring(0,2).equals("pi"))
+    	else if(s.substring(0,2).equals("pi"))
 			return "3.14"+replacePi(s.substring(2));
-
-		return s.charAt(0)+replacePi(s.substring(1));
+		// basic increment in recursion
+		else 
+			return s.charAt(0)+replacePi(s.substring(1));
     }
 }
